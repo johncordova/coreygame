ed98ccbe24ad2b5c043216281b737868710c2aec
John Cordova <johncordova@gmail.com>
2023-02-26 22:15:08 -0500

    added controls

--- main.js
+++ main.js
@@ -3,4 +3,11 @@ myCanvas.width=200;
 
 const ctx=myCanvas.getContext("2d");
 const car=new Car(100,100,30,50);
-car.draw(ctx);
+
+
+animate();
+function animate(){
+    car.update();
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
