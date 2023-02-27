aaefe837de8b4352d48f8a4efe50ae77573d3419
John Cordova <johncordova@gmail.com>
2023-02-26 23:02:07 -0500

    added car motion

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ myCanvas.width=200;
 const ctx=myCanvas.getContext("2d");
 const car=new Car(100,100,30,50);
 
-
 animate();
+
 function animate(){
+    // This clears the canvas each frame.
+    myCanvas.height=window.innerHeight;
     car.update();
     car.draw(ctx);
     requestAnimationFrame(animate);
