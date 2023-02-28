2ea0fbe82a69e0b55244a9fc9e9c4a4c3fb1d921
John Cordova <johncordova@gmail.com>
2023-02-27 20:35:23 -0500

    lanes

--- main.js
+++ main.js
@@ -2,16 +2,21 @@ myCanvas.height=window.innerHeight;
 myCanvas.width=200;
 
 const ctx=myCanvas.getContext("2d");
-const road=new Road(myCanvas.width/2, myCanvas.width,3);
-const car=new Car(100,100,30,50);
+const road=new Road(myCanvas.width/2, myCanvas.width*0.9,3);
+const car=new Car(road.getLaneCenter(1),100,30,50);
 
 animate();
 
 function animate(){
     // This clears the canvas each frame.
+    ctx.save();
+    //
     myCanvas.height=window.innerHeight;
+        // Have the camera follow the car
+        ctx.translate(0,-car.y+myCanvas.height*0.7);
     car.update();
     road.draw(ctx)
     car.draw(ctx);
+    ctx.restore();
     requestAnimationFrame(animate);
 }
--- road.js
+++ road.js
@@ -9,13 +9,28 @@ class Road{
         const infinity=10000;
         this.top=infinity;
         this.bottom=-infinity;
+
+        const topLeft={x:this.left, y:this.top};
+        const bottomLeft={x:this.left, y:this.bottom};
+        const topRight={x:this.right, y:this.top};
+        const bottomRight={x:this.right, y:this.bottom};
+
+        this.borders=[
+            [topLeft,  topRight]
+        ];
+
+    getLaneCenter(laneIndex){
+        const laneWidth=this.width/this.laneCount;
+        return this.left+laneWidth/2+
+        Math.min(laneIndex, this.laneCount-1)*laneWidth;
     }
+
 
     draw(ctx){
         ctx.linewidth = 5;
-        ctx.strokestyle="white";
+        ctx.strokeStyle="yellow";
         // This is where we draw all the lanes
-        for (let i=0;i<=this.laneCount;i++){
+        for (let i=1;i<=this.laneCount-1;i++){
             // We have defined a linear interpolation method in util.js
             const x=lerp(
                 this.left,
@@ -27,13 +42,19 @@ class Road{
                 ctx.setLineDash([20,20]);
             }
             else {
-                ctx.setLineDash[]
+                ctx.setLineDash([]);
             }
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
         }
+    this.borders.forEach{border=>(
+        ctx.beginPath();
+        ctx.moveTo(border[0].x,border[0].y);
+        ctx.lineTo(border[1].x,border[1].y);
+        ctx.stroke();
+    )
     }
 }
 
