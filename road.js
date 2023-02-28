c5c8e125b50dae884f18243681d0a7e43766c345
John Cordova <johncordova@gmail.com>
2023-02-28 09:43:00 -0500

    lanes

--- road.js
+++ road.js
@@ -7,17 +7,14 @@ class Road{
         this.right=x+width/2;
 
         const infinity=10000;
-        this.top=infinity;
-        this.bottom=-infinity;
+        this.top=-infinity;
+        this.bottom=infinity;
 
         const topLeft={x:this.left, y:this.top};
         const bottomLeft={x:this.left, y:this.bottom};
         const topRight={x:this.right, y:this.top};
         const bottomRight={x:this.right, y:this.bottom};
 
-        this.borders={
-            [topLeft, topRight];
-        }
     }
 
     getLaneCenter(laneIndex){
